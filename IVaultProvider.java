d7a407711a6b6dc99cb4d86f47c890f09613c070
liquidman228 <93479046+liquidman228@users.noreply.github.com>
2026-01-31 00:38:18 +0200

    Create IVaultProvider.java

---
+++ IVaultProvider.java
@@ -0,0 +1,11 @@
+package org.metadevs.buycraftapi.providers;
+
+import org.bukkit.OfflinePlayer;
+
+public interface IVaultProvider {
+    boolean setupPermissions();
+
+    String getPrimaryGroup(OfflinePlayer player);
+
+    boolean isHooked();
+}
